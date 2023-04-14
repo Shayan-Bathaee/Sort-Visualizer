61ae2d8a46fe8872812be7dd35830d8aef9ee001
Shayan-Bathaee <shayanbathaee@gmail.com>
2023-04-13 21:15:45 -0700

    finished user input checks and responses

--- sort.cpp
+++ sort.cpp
@@ -6,13 +6,16 @@
 #include <vector>  
 #include <stdio.h>
 #include <string>
+#include <algorithm>
 #include "sort_functions.h"
 using namespace std;
 
+#define NUM_INPUTS 3
 #define SCREEN_WIDTH 1200
 #define SCREEN_HEIGHT 800
+#define MAX_ARRAY_LEN SCREEN_WIDTH / 4
 
-vector<string> sort_types = {"sort 1", "sort 2"};
+vector<string> sort_types = {"sort-1", "sort-2"};
 
 /*
 Initializes SDL, creating the window.
@@ -53,12 +56,12 @@ void SDLclose(SDL_Window* window) {
 
 
 int main(int argc, char* argv[]) {
-   // take user input
-   if ((argc != 3) || ((string)argv[1] == "-h") || ((string)argv[1] == "-H")) { // user needs help
+   /* Take user input */
+   if (argc != NUM_INPUTS) { // user needs help
       cout << "Usage: " << endl;
       cout << "\t./sort <array-len> <sort-type>" << endl;
       cout << "Requirements:" << endl;
-      cout << "\t0 < array-len < " << SCREEN_WIDTH / 4 << endl;
+      cout << "\t0 < array-len <= " << MAX_ARRAY_LEN << endl;
       cout << "\tSort types: ";
       for (uint i = 0; i < sort_types.size(); i++) {
          if (i == sort_types.size() - 1) cout << sort_types[i] << endl;
@@ -66,6 +69,45 @@ int main(int argc, char* argv[]) {
       }
       return 0;
    }
+
+   // check array_len
+   string array_len_string = argv[1];
+   int i = 0;
+   while (array_len_string[i]) {
+      if (isdigit(array_len_string[i]) == false) {
+         cout << "ERROR: ";
+         cout << "array-len must be a positive integer" << endl;
+         return 0;
+      }
+      i++;
+   }
+   int array_len = stoi(array_len_string);
+   if (array_len == 0) {
+      cout << "ERROR: ";
+      cout << "array-len cannot be 0" << endl;
+      return 0;
+   }
+   if (array_len > MAX_ARRAY_LEN) {
+      cout << "ERROR: ";
+      cout << "array-len cannot exceed " << MAX_ARRAY_LEN << endl;
+      return 0;
+   }
+
+   // check sort type
+   string sort_type = argv[2];
+   if (find(sort_types.begin(), sort_types.end(), sort_type) == sort_types.end()) {
+      cout << "ERROR: ";
+      cout << sort_type << " is not a valid sort type" << endl;
+      cout << "Valid sort types: ";
+      for (uint i = 0; i < sort_types.size(); i++) {
+         if (i == sort_types.size() - 1) cout << sort_types[i] << endl;
+         else cout << sort_types[i] << ", ";
+      }
+      return 0;
+   }
+
+   
+
    
    // initialize window and screen surface variables
    SDL_Window* window = NULL;
