739e39b98d34b81e344beafab0de400db1a52b3d
Shayan-Bathaee <shayanbathaee@gmail.com>
2023-04-30 13:51:20 -0700

    Close window during sort added. Might change later for efficiency

--- sort.cpp
+++ sort.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <algorithm>
 #include <unistd.h>
+#include <thread>
 #include "sort_functions.h"
 
 #define ARRAY_SIZE 100
@@ -17,17 +18,23 @@ int main() {
    if (!SDLinit(&window, &renderer)) {
       cout << "Initialization failure" << endl;
    }
-
+   
    // build hardcoded array
    int a[ARRAY_SIZE];
    for (int i = 0; i < ARRAY_SIZE; i++) {
       a[i] = rand() % SCREEN_HEIGHT + 1;
    }
-   // display hardcoded array and start sorting
+   // draw array and sort it
    draw_array(&renderer, a, ARRAY_SIZE, -1);
-   bubble_sort(&renderer, a, ARRAY_SIZE);
+   bool sorted = bubble_sort(&renderer, a, ARRAY_SIZE);
    
-   // keep the window up
+   // user closed window
+   if (!sorted) {
+      SDLclose(&window, &renderer);
+      return 0;
+   }
+
+   // array was sorted, keep the window up
    SDL_Event e;
    bool quit = false;
    while(quit == false) {
@@ -37,6 +44,6 @@ int main() {
          }
       }
    }
-   SDLclose(&window, &renderer);
+   
    return 0;
 }
--- sort_functions.h
+++ sort_functions.h
@@ -2,7 +2,8 @@
 #include "SDL_functions.h"
 using namespace std;
 
-void bubble_sort(SDL_Renderer** renderer, int* a, int size) {
+bool bubble_sort(SDL_Renderer** renderer, int* a, int size) {
+    SDL_Event e;
     for (int j = 1; j < size; j++) {
         for (int i = 0; i < size - 1; i++) {
             if (a[i] > a[i + 1]) {
@@ -11,7 +12,13 @@ void bubble_sort(SDL_Renderer** renderer, int* a, int size) {
                 a[i + 1] = tmp;
                 draw_array(renderer, a, size, i + 1); // draw array with the moving element colored
             }
+            if (SDL_PollEvent(&e)) { // check if user wants to close window
+                if (e.type == SDL_QUIT) {
+                    return 0;
+                }
+            }
         }
     }
     draw_array(renderer, a, size, -1); // draw sorted array in white only
+    return 1;
 }
